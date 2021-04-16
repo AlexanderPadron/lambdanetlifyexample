b889cc49c156d001c103fef016d71446ced961b1
Alex <alex@iMac-de-pablo.local>
2021-04-16 12:45:35 -0400

    initial commit5

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 async function verIp(){
-    await (await fetch('./netlify/functions/getusers')).json()
-    //await (await fetch('http://localhost:9000/getusers')).json()
+    await (await fetch('/.netlify/functions/getusers')).json()
+   //await (await fetch('http://localhost:9000/getusers')).json()
     .then(data=>{
         console.log(data)
         document.getElementById("viewIp").innerHTML = data.origin;
